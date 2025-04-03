ebf49d9a0301543af5b46ced7e275da34d61e98c
kortney <kcanyon_2000@yahoo.com>
2025-04-02 20:16:36 -0500

    installed PostgreSQL/connected to express

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
 require ('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
+
 
 const app = express();
 
 app.use(express.json());
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.connect()
+.then(() => console.log("Connected to Database"))
+.catch(() => console.log("Error connecting to DB"))
 
 app.get('/', (req, res) => {
     res.send('API is up')
